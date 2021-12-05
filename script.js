38f6c6f76b59d4c3d70ff139c11b200f9f1497d1
Tenzin Jorden <tenden96@gmail.com>
2021-12-04 22:12:04 -0800

    Completed Personal Website

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Here's a few of my favorite things and some information about them!";
 
 document.querySelector("body").appendChild(h2);
+
